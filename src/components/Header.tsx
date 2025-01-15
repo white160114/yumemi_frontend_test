746b6686a45faf2fcc45d5b9c435c8c53f8111b8
matsuokayuuki <matsu.yu-0114@ezweb.ne.jp>
2025-01-15 13:15:35 +0900

    Headerを作成

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './Header.css';
+
+const Header: React.FC = () => {
+    return (
+        <header className="header">
+            yumemi
+        </header>
+    )
+}
